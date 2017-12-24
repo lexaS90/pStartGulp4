f15ec0bd15752c952ca9ddf633eb4b492b0ddedf
Alexy Stupakov <workinweb24@gmail.com>
2017-12-24 13:52:30 +0200

    Bootsrup

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,15 @@
+/**
+ * Uncomment if use bootstrup
+ */
+
+//  global.$ = require('jquery');
+//  global.jQuery = require('jquery');
+//  global.Popper = require('popper.js');
+//  require('bootstrap');
+
+/**
+ * Example
+ */
+
 var work = require('./_components/_test.js');
 work();
